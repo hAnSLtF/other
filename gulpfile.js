a2e814e9761b6db05789e6dd10416e237cb3f990
sunhan <sunhan@ctrip.com>
2018-04-08 20:06:24 +0800

    gulp-file-test

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,48 @@
-let gulp = require('gulp');
-let less = require('gulp-less');
+const gulp = require('gulp'),
+    less = require('gulp-less'),
+    browserSync = require('browser-sync'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css')
 
-gulp.task('less', () => {
-    gulp.src('app/src/test1/test1.less')
-        .pipe(less())
-        .pipe(gulp.dest('app/dest'));
+let path = './app/src/';
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: {
+            baseDir: './app/dest/test1/',
+            middleware: [
+                function (req, res, next) {
+                    console.log('refresh');
+                    next();
+                }
+            ]
+        },
+        port: 3080,
+        logPrefix: 'test',
+        logFileChanges: true,
+        logLevel: 'info',
+    });
 });
 
-gulp.task('default', gulp.series('less'));
+gulp.task('less', () => {
+    gulp.src(['app/src/**/*.less', '!app/src/node_modules/**'])
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['> 1%', 'last 2 versions', 'not ie < 8'],
+            remove: true
+        }))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('app/dest/'))
+});
+
+gulp.task('js', () => {
+    gulp.src('app/src/**/*.js')
+        .pipe(gulp.dest('app/dest/'))
+});
+
+
+gulp.task('default', gulp.series(['browser-sync', 'less']));
 
