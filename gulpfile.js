70fff789bf9567c3465beb9dd81d68a591a33ad4
sunhan <shsaul1008@gmail.com>
2018-07-03 17:25:39 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,7 @@ gulp.task('htmlmin', () => {
         removeComments: true, //清除HTML注释
         collapseWhitespace: true, //压缩HTML
         collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input checked />
-        removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+        removeEmptyAttributes: false, //删除所有空格作属性值 <input id="" /> ==> <input />
         removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
         removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
         minifyJS: true, //压缩页面JS
